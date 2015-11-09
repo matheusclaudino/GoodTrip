989ac540e14903d747c28097be8766538bb235c8
Matheus Claudino <matheusclaudin@gmai.com>
2015-11-09 13:53:30 -0200

    Criado os arquivos para a nova tela, chamada DashBoard.

---
+++ app/src/main/java/com/example/mclaudino/goodtrip/DashBoardActivity.java
@@ -0,0 +1,14 @@
+package com.example.mclaudino.goodtrip;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DashBoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard);
+
+    }
+}
