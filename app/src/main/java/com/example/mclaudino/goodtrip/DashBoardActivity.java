de8c1f751edfe36efb5a0ea7143e72bbcaf51a40
Matheus Claudino <matheusclaudin@gmai.com>
2015-11-09 15:58:46 -0200

    Criada a Activity DashBoard.

--- app/src/main/java/com/example/mclaudino/goodtrip/DashBoardActivity.java
+++ app/src/main/java/com/example/mclaudino/goodtrip/DashBoardActivity.java
@@ -2,6 +2,9 @@ package com.example.mclaudino.goodtrip;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class DashBoardActivity extends AppCompatActivity {
 
@@ -9,6 +12,12 @@ public class DashBoardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dashboard);
+    }
 
+    public void selecionarOpcao(View view){
+
+        TextView textView = (TextView) view;
+        String opcao = "Opção: " + textView.getText().toString();
+        Toast.makeText(DashBoardActivity.this, opcao, Toast.LENGTH_SHORT).show();
     }
 }
