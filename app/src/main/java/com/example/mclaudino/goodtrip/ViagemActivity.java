b6c57baf7911170d5535cbb7f44a4627afcda1c0
Matheus Claudino <matheusclaudin@gmai.com>
2015-11-09 23:45:42 -0200

    Criada a Activity da tela de nova viagem.

---
+++ app/src/main/java/com/example/mclaudino/goodtrip/ViagemActivity.java
@@ -0,0 +1,14 @@
+package com.example.mclaudino.goodtrip;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ViagemActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cadastro_viagem);
+    }
+
+}
