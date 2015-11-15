2522977ec29fb16204050ca9c5ed6ab0da9d06f3
Matheus Claudino <matheusclaudin@gmai.com>
2015-11-15 10:21:52 -0200

    Adicionado o menu com opção de criar um novo gasto e remover viagem, acionado via ação da opção menu.

--- app/src/main/java/com/example/mclaudino/goodtrip/ViagemActivity.java
+++ app/src/main/java/com/example/mclaudino/goodtrip/ViagemActivity.java
@@ -1,7 +1,13 @@
 package com.example.mclaudino.goodtrip;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import java.util.zip.Inflater;
 
 public class ViagemActivity extends Activity {
 
@@ -11,4 +17,24 @@ public class ViagemActivity extends Activity {
         setContentView(R.layout.cadastro_viagem);
     }
 
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.viagem_menu, menu);
+        return true;
+    }
+
+    public boolean onMenuItemSelected(int featuredId, MenuItem item){
+        switch (item.getItemId()){
+            case R.id.novo_gasto:
+                startActivity(new Intent(this, GastoActivity.class));
+                return true;
+
+            case R.id.remover:
+                //remover do banco de dados
+                return true;
+
+            default:
+                return super.onMenuItemSelected(featuredId, item);
+        }
+    }
 }
