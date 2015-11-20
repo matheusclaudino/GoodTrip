7215ad8e57bff634134dba152097993c52b69dcd
Matheus Claudino <matheusclaudin@gmai.com>
2015-11-20 02:54:49 -0200

    Criado referência para as views com informação da Activity de cadastro de viagem.

--- app/src/main/java/com/example/mclaudino/goodtrip/ViagemActivity.java
+++ app/src/main/java/com/example/mclaudino/goodtrip/ViagemActivity.java
@@ -6,15 +6,42 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
+import java.util.Calendar;
 import java.util.zip.Inflater;
 
 public class ViagemActivity extends Activity {
+    private DataBaseHelper helper;
+    private EditText destino, qtdPessoas, orcamento;
+    private RadioGroup radioGroup;
+    private int ano, mes, dia;
+    private Button dataChegada, dataSaida;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cadastro_viagem);
+
+        //pegando data atual do sistema
+        Calendar calendar = Calendar.getInstance();
+        ano = calendar.get(Calendar.YEAR);
+        mes = calendar.get(Calendar.MONTH);
+        dia = calendar.get(Calendar.DAY_OF_MONTH);
+
+        //criando referências para as views que contém os dados informados pelo usuário
+        dataChegada = (Button) findViewById(R.id.data_chegada);
+        dataSaida = (Button) findViewById(R.id.data_saida);
+
+        destino = (EditText) findViewById(R.id.destino);
+        qtdPessoas = (EditText) findViewById(R.id.qtd_pessoas);
+        orcamento = (EditText) findViewById(R.id.orcamento);
+        radioGroup = (RadioGroup) findViewById(R.id.tipo_viagem);
+
+        //prepara acesso ao banco de dados
+        helper = new DataBaseHelper(this);
     }
 
     public boolean onCreateOptionsMenu(Menu menu){
